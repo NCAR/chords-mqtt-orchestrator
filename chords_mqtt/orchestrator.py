78e05a15fff11d1ad1ec481d0d32f0501951f536
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-01-15 14:31:24 -0700

    Formatting only

--- chords_mqtt/orchestrator.py
+++ chords_mqtt/orchestrator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import paho.mqtt.client as mqtt
-import yaml
 import requests
+import yaml
+
 from .config import config as cfg
+
 # todo : add logging
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print('Connected with result code ' + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -30,21 +32,24 @@ def on_message(client, userdata, msg):
             var = instruments[sensor].get(measurement, False)
 
         sensor_id = data['device'].split('/')[1]
-        parameters = "sensor_id={}&{}={}&email={}&api_key={}&test" \
-            .format(sensor_id,
-                    var,
-                    data['m'],
-                    email,
-                    api_key)
+        parameters = 'sensor_id={}&{}={}&email={}&api_key={}&test'.format(
+            sensor_id, var, data['m'], email, api_key
+        )
 
-        r = requests.get("{}/measurements/url_create?{}".format(cfg['chords']['base_api_endpoint'], parameters))
+        r = requests.get(
+            '{}/measurements/url_create?{}'.format(cfg['chords']['base_api_endpoint'], parameters)
+        )
 
         if r.status_code == 200:
-            print("[info]: data successfully sent to chords at {}".format(cfg["chords"]["base_api_endpoint"]))
+            print(
+                '[info]: data successfully sent to chords at {}'.format(
+                    cfg['chords']['base_api_endpoint']
+                )
+            )
         else:
             print('[warn]: {} - {}'.format(r.status_code, r.content))
     except Exception as e:
-        print("[error]: {}".format(e))
+        print('[error]: {}'.format(e))
 
 
 def main():
@@ -52,7 +57,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect(cfg['mqtt']['host'],  cfg['mqtt']['port'], 60)
+    client.connect(cfg['mqtt']['host'], cfg['mqtt']['port'], 60)
     client.loop_forever()
 
     # todo : add testing
